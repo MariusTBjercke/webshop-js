73df2465912be0383d7ad227f6ccdef19f7c9cd0
MariusTBjercke <marius@bjerckemedia.no>
2021-10-18 23:59:55 +0200

    Began adding authentication

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-analytics.js";
+import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -27,6 +28,31 @@ import {
 
 // Initialize database
 const db = getFirestore();
+
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+auth.languageCode = 'it';
+provider.setCustomParameters({
+  'login_hint': 'user@example.com'
+});
+signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
 
 // class TestItem {
 //   constructor (name, state, country) {
